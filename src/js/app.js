1ee195ab07529b3cdf03f3c760efb17fee6a8623
Marquinhus Goncalves <eu@marquinhusgoncalves.com>
2017-10-18 21:28:25 -0200

    Test new structure files JS

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+// 'use strict';
+// //dependencias do modulo
+// var responsiveMenu = require('./responsiveMenu');
+// var flickity       = require('./flickity');
+// var scrollToTop    = require('./scrollToTop');
+// var analytics      = require('./analytics');
+
+// var app = {
+//   init: function() {
+//     console.log('app init()');
+//   }
+// };
+
+// app.init();
